a6f0c6d629118650e06b657042ed0d97cdcca539
tracer-ics2017 <tracer@njuics.org>
2022-03-17 05:25:19 -0700

    >  compile 1910378 fyz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  05:25:19 up 25 min,  1 user,  load average: 0.56, 0.35, 0.20 3dd18053ebab8e66985154d986e4ed9813ad3058

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -4,25 +4,56 @@
 #define NR_WP 32
 
 static WP wp_pool[NR_WP];
-static WP *head, *free_;
+static WP *head, *free_,*wptemp;
+static int used_next;
 
 void init_wp_pool() {
   int i;
   for (i = 0; i < NR_WP; i ++) {
     wp_pool[i].NO = i;
     wp_pool[i].next = &wp_pool[i + 1];
+    wp_pool[i].oldValue=0;
+    wp_pool[i].hitNum=0;
   }
   wp_pool[NR_WP - 1].next = NULL;
 
   head = NULL;
   free_ = wp_pool;
+  used_next=0;
 }
 
 /* TODO: Implement the functionality of watchpoint */
 
+bool new_wp(char *args) 
+{
+	if (free_ == NULL) assert(0);
+	WP *temp = free_;
+	free_ = free_->next;
+	
+	temp->hitNum = 0;
+
+	temp->NO = used_next;used_next++;
+	temp->next = NULL;
+	strcpy(temp->e, args);
+	bool success;
+	temp->oldValue=expr(temp->e,&success);
+	if(success==false)
+	{printf("err in new_wp:invalid expr!!\n");return 0;}
+
+	wptemp = head;
+	if (wptemp == NULL) head = temp;
+	else 
+	{
+		while (wptemp->next != NULL) wptemp = wptemp->next;
+		wptemp->next = temp;
+	}
+	printf("success:set wp %d,oldValue=%d\n",temp->NO,temp->oldValue);
+	return 1;
+}
+
 void print_wp()
 {
-  if(head==NULL){printf("no watchpoint now\n");return n;}
+  if(head==NULL){printf("no watchpoint now\n");return;}
   printf("watchpoint:\n");
   printf("NO.    EXPR        hitTimes\n");
   wptemp=head;
