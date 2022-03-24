446614947c077ba5498aba86082f4dfca80d6355
tracer-ics2017 <tracer@njuics.org>
2022-03-24 04:40:10 -0700

    >  compile 1910378 fyz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  04:40:10 up 55 min,  1 user,  load average: 0.19, 0.25, 0.20 6402d1621cb90d596a3265414b50fc56e478108a

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -7,7 +7,12 @@ make_EHelper(test) {
 }
 
 make_EHelper(and) {
-  TODO();
+  //TODO();
+  rtl_and(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
+  rtl_update_ZFSF(&t0, id_dest->width);
+  rtl_set_CF(&tzero); // CF = 0
+  rtl_set_OF(&tzero); // OF = 0
 
   print_asm_template2(and);
 }
