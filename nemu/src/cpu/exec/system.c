3164b6afd95592efe9ac874bc62892b120e6c7d4
tracer-ics2017 <tracer@njuics.org>
2022-03-29 07:14:52 -0700

    >  compile 1910378 fyz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  07:14:52 up 14 min,  1 user,  load average: 0.83, 0.49, 0.33 c9337ae35d62aba29b70a86ae380b195fc3895bd

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -47,7 +47,7 @@ void pio_write(ioaddr_t, int, uint32_t);
 make_EHelper(in) {
   //TODO();
   rtl_li(&t0,pio_read(id_src->val,id_dest->width));
-  operand_wirte(id_dest,&t0);
+  operand_write(id_dest,&t0);
 
   print_asm_template2(in);
 
