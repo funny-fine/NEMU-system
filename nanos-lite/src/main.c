967adde7296a154cb0f85e267f1dd4d6b4aec9bd
tracer-ics2017 <tracer@njuics.org>
2022-04-16 21:05:54 -0700

    >  run 1910378 fyz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:05:54 up  1:04,  1 user,  load average: 0.75, 0.51, 0.38 d30ed4b5b72c3f15fdebbcd1f032a4a05db229d8

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -30,7 +30,7 @@ int main() {
 
   init_fs();
 
-  uint32_t entry = loader(NULL, "/bin/events");
+  uint32_t entry = loader(NULL, "/bin/pal");
   //uint32_t entry = loader(NULL, NULL);
   ((void (*)(void))entry)();
 
